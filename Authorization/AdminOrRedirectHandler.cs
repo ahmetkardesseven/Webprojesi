e89729d051d2a1d2341d18e5c30be4cc6d0d828e
ahmetkardesseven <103737133+ahmetkardesseven@users.noreply.github.com>
2024-01-15 14:07:01 +0300

    Add files via upload
    ufak eksikler duzeltildi

---
+++ Authorization/AdminOrRedirectHandler.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace PlaneTicket.Authorization;
+
+public class AdminOrRedirectHandler : AuthorizationHandler<AdminOrRedirectRequirement>
+{
+    protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, AdminOrRedirectRequirement requirement)
+    {
+        if (!context.User.Identity.IsAuthenticated)
+        {
+            context.Fail();
+            return Task.CompletedTask;
+        }
+
+        if (context.User.IsInRole("Admin"))
+        {
+            context.Succeed(requirement);
+        }
+        else
+        {
+            context.Fail();
+        }
+
+        return Task.CompletedTask;
+    }
+}
